0f366f729c55c5314621a9cb5c3eb86ee57e720b
gourishchouhan <gourishchouhan338@gmail.com>
2025-07-26 20:05:46 +0530

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
